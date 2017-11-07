ecdaca9bd0d3aac701439cb33934c4d2f64f715b
user <jiayou.zuo@corp.elong.com>
2017-11-07 17:20:07 +0800

    first commit

---
+++ src/components/MessageBox/MessageBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="iuc_message_box__wrap"></div>
+</template>
+
+<script>
+export default {
+    name: 'MessageBox',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.iuc_message_box__wrap{
+    
+}
+</style>
---
+++ src/components/MessageBox/index.js
@@ -0,0 +1,11 @@
+/**
+* MessageBox Components :
+* Created by Jiayou Zuo on 2017/11/07
+*
+*/
+import Vue from 'vue';
+import MessageBox from './MessageBox.vue';
+
+MessageBox.install = (Vue) => Vue.Component('MessageBox',MessageBox);
+
+export default MessageBox;
