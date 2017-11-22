2eb18ba59f35014314970e989fa819e3829cc179
user <jiayou.zuo@corp.elong.com>
2017-11-22 20:32:40 +0800

    组件

--- src/components/Button/index.js
+++
@@ -1,5 +0,0 @@
-import IUCButton from './src/button';
-
-IUCButton.install = (Vue) => Vue.component(IUCButton.name, IUCButton);
-
-export default IUCButton;
--- src/components/MessageBox/index.js
+++
@@ -1,9 +0,0 @@
-/**
-* MessageBox Components :
-* Created by Jiayou Zuo on 2017/11/07
-*
-*/
-import MessageBox from './src/main.js';
-// MessageBox.install = (Vue) => Vue.Component(MessageBox.name,MessageBox);
-
-export default MessageBox;
